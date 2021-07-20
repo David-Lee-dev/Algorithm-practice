a58ddc9c6a356971d8e4964eea78f24e03f68e7f
JOOHYEON123 <aganga7427@gmail.com>
2021-07-20 21:25:33 +0900

    swea #1926

---
+++ simple_369game.py
@@ -0,0 +1,18 @@
+def change(i):
+    result = ''
+    for j in i:
+        if (j == '3' or j == '6' or j == '9'):
+            j = '-'
+            result += j
+        
+    return result
+
+N = int(input())
+
+for i in range(1, int(N)+1):
+    for j in str(i):
+        if (j == '3' or j == '6' or j == '9'):
+            i = change(str(i))
+            break
+        i = str(i)
+    print(i, end=' ')
