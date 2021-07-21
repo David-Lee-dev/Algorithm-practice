8d87b7171781795795344e358f9838cf7607bc01
JOOHYEON123 <aganga7427@gmail.com>
2021-07-21 23:06:38 +0900

    swea #1989

---
+++ D2/palindrome.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = input()
+    tmp = len(N)//2
+
+    buf_1 = N[:tmp]
+    buf_2 = N[-tmp:]
+    
+    for i in range(len(buf_1)):
+        if(buf_1[i] != buf_2[-i-1]):
+            print('#{} 0'.format(test_case))
+            break
+    else:
+        print('#{} 1'.format(test_case))
