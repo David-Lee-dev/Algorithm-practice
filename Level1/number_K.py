c7e101f8e77521bc260ae31b10126038e2cb7f23
이주현 <aganga7427@gmail.com>
2021-07-24 22:11:43 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42748

---
+++ Level1/number_K.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        tmp_array = array[command[0]-1:command[1]]
+        answer += [sorted(tmp_array)[command[2]-1]]
+    return answer
