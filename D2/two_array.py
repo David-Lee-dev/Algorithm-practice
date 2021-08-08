68eecfc1760b563d06bc78b609c47e0a02a11dd8
Ju Hyeon <aganga7427@gmail.com>
2021-08-08 14:06:38 +0900

    swea #1959

---
+++ D2/two_array.py
@@ -0,0 +1,23 @@
+import copy
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M = map(int, (input().split()))
+    Ai = list(map(int, input().split()))
+    Bj = list(map(int, input().split()))
+
+    if N > M:
+        N, M = M, N
+        Ai, Bj = Bj, Ai
+
+    result = 0
+
+    for m in range(0, M-N+1):
+        num = 0
+        for n in range(N):
+            num += Ai[n] * Bj[n+m]
+
+        if result < num:
+            result = num
+
+    print("#{} {}".format(test_case, result))
