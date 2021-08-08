3315984e27a48f7fd1326fa36a930f5adf8c5a02
Ju Hyeon <aganga7427@gmail.com>
2021-08-08 15:39:10 +0900

    swea #1940

---
+++ D2/go_RCcar.py
@@ -0,0 +1,20 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    commends = []
+    for i in range(N):
+        commends.append(list(map(int, input().split())))
+
+    distance = 0
+    speed = 0
+    mean = [0, 1, -1]
+    for commend in commends:
+        if commend[0] != 0:
+            speed += mean[commend[0]]*commend[1]
+            if speed < 0:
+                speed = 0
+            distance += speed
+        else:
+            distance += speed
+
+    print("#{} {}".format(test_case, distance))
